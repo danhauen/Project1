d8a18e5e1a360448e3c71404ca7643eb6f914b8e
hallieraew <hallieweintraub@gmail.com>
2020-01-25 13:24:59 -0600

    commiting my work before pulling

--- assets/javscript/app.js
+++ assets/javscript/app.js
@@ -1,18 +1,26 @@
-$("#button-addon1").on("click", function(){
+$("#button-addon1").on("click", function () {
     console.log("clicked");
     var userSearch = $("#userInputLeft").val().trim();
     var superHero = userSearch;
-    
+
     var queryURL = "https://superheroapi.com/api/10213837355721301/search/" + superHero;
 
     console.log(queryURL);
 
     $.ajax({
         url: queryURL,
-        method: "GET"
+        method: "GET",
+        dataType: "jsonp",
+        headers: { 'Access-Control-Allow-Origin': 'https://superheroapi.com/api/10213837355721301/search/' },
+        crossDomain: true,
     })
-    .then(function (results) {
-        var results = results.powerstats;
-        console.log(results);
-    })
+
+        .then(function (response) {
+            console.log(results.name);
+            var response = results;
+            console.log(response);
+            $("#name1").text(results.name);
+        })
+
 });
+
