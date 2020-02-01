621afc53f9165e435c30127e143e56a36d921639
normalmn <appliedboredom@gmail.com>
2020-02-01 11:31:51 -0600

    better animations

--- assets/javscript/app.js
+++ assets/javscript/app.js
@@ -2,6 +2,11 @@
 $(document).ready(function () {
     $("#loveBtnReload").hide();
     $("#btn-newchallenge").hide();
+
+    $(".hero-left").addClass("animated fadeInLeftBig");
+    $(".hero-right").addClass("animated fadeInRightBig");
+    $(".compat-card").addClass("animated fadeInUpBig");
+    $(".challenge-select").addClass("animated fadeInDownBig");
    
    
     // challenge active
@@ -11,8 +16,8 @@ $(document).ready(function () {
     });
 
     // modal
-    $('#myModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus')
+    $('#results-modal').on('shown.bs.modal', function () {
+        $('#btn-challenge').trigger('focus')
       })
 
     var responseIndex = -1;
@@ -84,23 +89,27 @@ $(document).ready(function () {
     function compareStat(stats) {
         stats = stats[0] - stats[1];
         if (stats > 0) {
-            $(".hero-right").addClass("animated fadeOutRightBig")
-            // $(".challenge-select").addClass("animated fadeOutUpBig")
-            // $(".compat-card").addClass("animated fadeOutDownBig")
-            // $("#btn-reset").hide();
+            $(".hero-right").removeClass("fadeInRightBig").addClass("fadeOutRightBig")
+            //$(".challenge-select").removeClass("fadeOutUpBig").addClass("fadeOutUpBig")
+            $(".compat-card").removeClass("fadeInUpBig").addClass("fadeOutDownBig")
+            $("#btn-reset").hide();
 
         }
         else if (stats < 0) {
-            $(".hero-left").addClass("animated fadeOutLeftBig")
-            // $(".challenge-select").addClass("animated fadeOutUpBig")
-            // $(".compat-card").addClass("animated fadeOutDownBig")
-            // $("#btn-reset").hide();
+            $(".hero-left").removeClass("fadeInLeftBig").addClass("fadeOutLeftBig")
+            //$(".challenge-select").removeClass("fadeInDownBig").addClass("fadeOutUpBig")
+            $(".compat-card").removeClass("fadeInUpBig").addClass("fadeOutDownBig")
+            $("#btn-reset").hide();
         }
         else {
+            $(".hero-left").removeClass("fadeInLeftBig").addClass("fadeOutLeftBig")
+            $(".hero-right").removeClass("fadeInRightBig").addClass("fadeOutRightBig")
+            $(".compat-card").removeClass("fadeInUpBig").addClass("fadeOutDownBig")
+            $("#btn-reset").hide();
             console.log("tie");
         };
     };
-    // challenge function btn in challeng list
+    // challenge function btn in challenge list
     $("#btn-challenge").click(function () {
         $("#btn-challenge").hide();
         $("#btn-newchallenge").show();
@@ -131,7 +140,13 @@ $(document).ready(function () {
 
     // new challenge btn
     $("#btn-newchallenge").click(function () {
-        location.reload(true);
+        $(".fadeOutLeftBig").removeClass("fadeOutLeftBig").addClass("fadeInLeftBig");
+        $(".fadeOutRightBig").removeClass("fadeOutRightBig").addClass("fadeInRightBig");
+        $(".compat-card").removeClass("fadeOutDownBig").addClass("fadeInUpBig");
+        $("#btn-newchallenge").hide();
+        $("#btn-challenge").show();
+        $("btn-reset").show();
+
     })
 
     // love calculator api
@@ -159,7 +174,7 @@ $(document).ready(function () {
             console.log(settings.url);
         })
         // challenge selections fade up with love test
-        $(".challenge-select").addClass("animated fadeOutUpBig")
+        $(".challenge-select").removeClass("fadeInDownBig").addClass("animated fadeOutUpBig")
     
     });
     // reload/reset love btn
